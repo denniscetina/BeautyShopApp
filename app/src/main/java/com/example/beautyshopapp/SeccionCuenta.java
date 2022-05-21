34001e00f26d94f0d1fe5780f3fad1e7d9155141
Fanny May <misyesenia12@gmail.com>
2022-05-21 18:52:28 -0500

    SeccionCuenta

---
+++ app/src/main/java/com/example/beautyshopapp/SeccionCuenta.java
@@ -0,0 +1,14 @@
+package com.example.beautyshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeccionCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seccion_cuenta);
+    }
+}
